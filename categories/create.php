5fe7dd65e2f81f1370d0c9953da88b269ebdc772
Phone Myat Thu <myat57116@gmail.com>
2023-12-22 00:20:45 +0630

    including CategoriesController

--- categories/create.php
+++ categories/create.php
@@ -1,27 +1,36 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Create Products</title>
+    <!-- bootstrap cdn css1 js1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-<body>
-    <div class="w-[40%] mx-auto shadow-lg my-12 py-5">
-        <h1 class="text-center">Create Category</h1>
-        <form class="w-[90%] mx-auto" action="store.php" method="POST">
-            <div class="my-3">
-                <label for="name">Name</label>
-                <input required type="text" name="name" class="w-full border-2 border-blue-600 px-5 py-2">
+<body class="bg-light">
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-lg-5 m-auto shadow rounded p-4 bg-dark text-white">
+                <h3 class="text-center">Create Category</h3>
+                <p class="text-center text-white lead">Let's create new category</p>
+            
+                <!-- form start  -->
+                <form action="./store.php" method="POST">
+                    <div class="col-12">
+                        <label for="name" class="text-uppercase my-2" style="font-weight: bold;">Name</label>
+                        <input type="text" id="name" name="name" class="form-control form-control-sm" autocomplete="off"/>
+                    </div>
+                    <div class="d-flex justify-content-">
+                        <a class="btn btn-primary btn-sm w-100 mt-3 me-2">Save</a>
+                        <a href="./index.php" class="btn btn-warning btn-sm w-100 mt-3 ms-2">Back</a>
+                    </div>
+                </form>
+                <!-- form end -->
             </div>
-            <button class="w-full py-2 bg-blue-600 text-white my-3 rounded-md">Save</button>
-            <div class="my-2 py-2 w-full bg-gray-800 text-white rounded-md text-center">
-                <a href="index.php" class="w-full">Back</a>
-            </div>
-        </form>
+        </div>
     </div>
+
+    <!-- bootstrap cdn css1 js1  -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
-
---
+++ controller/CategoriesController.php
@@ -0,0 +1,32 @@
+<?php
+    require_once("../helper/database.php");
+    
+    class CategoriesController extends DB{
+        function index(){
+            $statement = $this->pdo->query('select * from categories');
+            $products = $statement->fetchAll(PDO::FETCH_OBJ);
+            return $products;
+        }
+
+        function store($request){
+            try{
+                $statement = $this->pdo->prepare('
+                    INSERT INTO categories (name)
+                    VALUES (:name);
+                ');
+                $statement->bindParam(':name', $request["name"]);
+                if($statement->execute()){
+                    header("Location: http://localhost:8080/categories/index.php");
+                }else{
+                    throw new Exception("Error Occurs creating categories");
+                }
+            }
+            catch(PDOException $e){
+                echo $e->getMessage();
+            }
+            catch(Exception $e){
+                echo $e->getMessage();
+            }
+        }
+    }
+?>
