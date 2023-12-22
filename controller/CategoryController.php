f1fd3a4a57c42857970bdee9d73afdfd3b8b8ad5
Okaburma <novstitch@gmail.com>
2023-12-22 20:27:55 +0630

    store again

--- controller/CategoryController.php
+++ controller/CategoryController.php
@@ -6,18 +6,15 @@ class categoryController extends DB{
         try{
             $statement = $this->pdo->prepare("
             insert into categories
-                (name, price, stock, category, created_at, updated_at)
+                (name, created_at, updated_at)
             values
-                (:name, :price, :stock, :category, now(), now())                
+                (:name, now(), now())                
             ");
 
             $statement->bindParam(":name",$request["name"]);
-            $statement->bindParam(":price",$request["price"]);
-            $statement->bindParam(":stock",$request["stock"]);
-            $statement->bindParam(":category",$request["category"]);
 
            if( $statement->execute()){
-                header("Location: http://localhost:8000/categories");
+                header("Location: http://127.0.0.1:8000/categories");
            }else{
                 throw new Exception("Error while storing data to Database");
            }
