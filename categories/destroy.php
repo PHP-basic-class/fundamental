8f85d95ecc56fbdeec1dca26092d42d21b913970
myozinoo1122 <myo910967@gmail.com>
2023-12-25 21:08:11 +0630

    destroy

--- categories/destroy.php
+++ categories/destroy.php
@@ -3,4 +3,4 @@ require_once "../controller/CategoryController.php";
 
 #Destroy Categories
 $controller = new CategoryController();
- $category= $controller->destroy($_GET['id']);
+      $category= $controller->destroy($_GET['id']);
