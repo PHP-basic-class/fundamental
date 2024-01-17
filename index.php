016c97e59bc31c7a833ffd04ab63cf3b4207a390
Kaung Myat Thu <kauntmt.dev@Km-Macbook-Pro.lan>
2024-01-17 22:01:07 +0630

    Created kauntmt.dev in index page

--- index.php
+++ index.php
@@ -7,9 +7,10 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <div class="mx-auto w-[40%] text-center my-10">
+    <div class="mx-auto w-[50%] text-center my-10">
         <a href="categories/index.php" class="px-10 py-3 bg-green-700 text-white mx-5">Category</a>
         <a href="products/index.php" class="px-10 py-3 bg-green-700 text-white mx-5">Products</a>
+        <a href="products/index.php" class="px-10 py-3 bg-green-700 text-white mx-5">Users</a>
     </div>
 </body>
 </html>
